30cac0f7ec9aa0f76287304b33876d8fd2685950
Asror Qayumov <qayumovasror2003@gmail.com>
2022-05-26 16:05:38 +0500

    :rocket: UI has full finished

--- js/plugins.js
+++ js/plugins.js
@@ -1,12 +1,32 @@
 // Avoid `console` errors in browsers that lack a console.
-(function() {
+(function () {
   var method;
   var noop = function () {};
   var methods = [
-    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    "assert",
+    "clear",
+    "count",
+    "debug",
+    "dir",
+    "dirxml",
+    "error",
+    "exception",
+    "group",
+    "groupCollapsed",
+    "groupEnd",
+    "info",
+    "log",
+    "markTimeline",
+    "profile",
+    "profileEnd",
+    "table",
+    "time",
+    "timeEnd",
+    "timeline",
+    "timelineEnd",
+    "timeStamp",
+    "trace",
+    "warn",
   ];
   var length = methods.length;
   var console = (window.console = window.console || {});
@@ -19,6 +39,35 @@
       console[method] = noop;
     }
   }
-}());
+})();
 
-// Place any jQuery/helper plugins in here.
+// card
+let buy = document.querySelectorAll(".buy");
+let remove = document.querySelectorAll(".remove");
+buy.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let bottom = e.target.parentElement.parentElement.parentElement;
+    bottom.classList.add("clicked");
+  });
+});
+remove.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let bottom = e.target.parentElement.parentElement.parentElement;
+    bottom.classList.remove("clicked");
+  });
+});
+
+// modal
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelectorAll(".close-btn");
+
+modalBtn.addEventListener("click", function () {
+  modal.classList.add("open-modal");
+});
+
+closeBtn.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    modal.classList.remove("open-modal");
+  });
+});
